057ceb2ee44bd9e5d3bc4351b4e37909dcf9e0c8
straup <copea@si.edu>
2014-06-05 13:33:57 -0400

    add docs

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-py-unicodecsv
---
+# py-cooperhewitt-csv
 
 This is Cooper-Hewitt's Python library for reading and writing CSV files and managing (most of) the Unicode pain. It is cobbled together from a variety of other people's code.
 
-It needs things like a `setup.py` file and other yummy details.
+## To do
+
+* Documentation
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cooperhewitt-csv',
+      version='0.1',
+      description='Tools for processing CSV files',
+      author='Smithsonian Cooper-Hewitt National Design Museum',
+      url='https://github.com/cooperhewitt/py-cooperhewitt-csv',
+      requires=[],
+      packages=[
+          'cooperhewitt'
+      ],
+      scripts=[],
+      download_url='https://github.com/cooperhewitt/py-cooperhewitt-csv/releases/tag/v0.1',
+      license='BSD')
